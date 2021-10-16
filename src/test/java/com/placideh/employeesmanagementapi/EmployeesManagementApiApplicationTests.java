287cc3b4ad555dc212a915ecb3399031b2469e3a
Placideh <h.uwizeyeplacide@gmail.com>
2021-10-17 00:57:32 +0200

    Added README file

--- src/test/java/com/placideh/employeesmanagementapi/EmployeesManagementApiApplicationTests.java
+++ src/test/java/com/placideh/employeesmanagementapi/EmployeesManagementApiApplicationTests.java
@@ -1,13 +1,23 @@
 package com.placideh.employeesmanagementapi;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class EmployeesManagementApiApplicationTests {
-
+	Calculator calculator=new Calculator();
 	@Test
-	void contextLoads() {
+	void itShouldAddNumbers() {
+		int a=20;
+		int b=30;
+		int result=calculator.add(a,b);
+//		then
+		Assertions.assertThat(result).isEqualTo(50);
 	}
-
+	class Calculator{
+		int add(int a ,int b){
+			return a+b;
+		}
+	}
 }
